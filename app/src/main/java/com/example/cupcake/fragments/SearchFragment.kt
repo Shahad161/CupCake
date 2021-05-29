8291c50e467360a87997f6aeb73a35042343a4e7
Sahar Qais <sahar.qais.k@gmail.com>
2021-05-30 02:14:41 +0300

    make BaseFragment

---
+++ app/src/main/java/com/example/cupcake/fragments/SearchFragment.kt
@@ -0,0 +1,11 @@
+package com.example.cupcake.fragments
+
+import com.example.cupcake.databinding.FragmentSearchBinding
+
+class SearchFragment: BaseFragment<FragmentSearchBinding>() {
+    override fun getViewBinding() = FragmentSearchBinding.inflate(layoutInflater)
+    override fun setUpViews() {
+
+        // set up recycler view and bind data to UI
+    }
+}
