931fcfc7e5b388e2c135ea4a1750b70043dffe87
Akram <akramtk@yahoo.com>
2021-06-01 02:50:54 +0300

    add toast for test

--- app/src/main/java/com/example/cupcake/fragments/ModelAdapter.kt
+++ app/src/main/java/com/example/cupcake/fragments/ModelAdapter.kt
@@ -3,14 +3,17 @@ package com.example.cupcake.fragments
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cupcake.R
+import com.example.cupcake.R.layout.item_model
 import com.example.cupcake.databinding.ItemModelBinding
 import com.example.cupcake.model.Model
 
 class ModelAdapter(val list: List<Model>): RecyclerView.Adapter<ModelAdapter.modelViewHolders>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): modelViewHolders {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_model, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(item_model, parent, false)
         return modelViewHolders(view)
     }
 
@@ -21,6 +24,9 @@ class ModelAdapter(val list: List<Model>): RecyclerView.Adapter<ModelAdapter.mod
             populationMax.text = currentModel.population.toString()
             longitudeMaxNum.text = currentModel.lon.toString()
             LatitudeMaxNum.text = currentModel.lat.toString()
+            countryMax.setOnClickListener{
+                Toast.makeText(countryMax.context, currentModel.country,Toast.LENGTH_SHORT)
+            }
         }
 
     }
